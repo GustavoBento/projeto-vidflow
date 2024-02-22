a1249cd55556c32bceadb7aaa3403adc02905e13
GustavoBento <gustavo_grb-@hotmail.com>
2024-02-21 21:14:05 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 const containerVideos = document.querySelector('.videos__container')
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) => 
-    videos.forEach(video => {
-        containerVideos.innerHTML += `
-            <li class="videos__item"> 
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                    <div class="descricao-video">
-                        <img class="img-canal" src="${video.imagem}"> </img>
-                        <h3 class="titulo-video"> ${video.titulo} </h3>
-                        <p class = "titulo-canal"> ${video.descricao}</p>
-                    </div>
-            </li>
-        `
+async function buscarEMostrarVideos() {
+    try {
+        const busca = await fetch("http://localhost:3000/videos")
+        const videos = await busca.json()
+            videos.forEach(video => {
+                if(video.categoria == ''){
+                    throw new Error('Video nao tem categoria')
+                }
+
+                containerVideos.innerHTML += `
+                    <li class="videos__item"> 
+                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                            <div class="descricao-video">
+                                <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                                <h3 class="titulo-video"> ${video.titulo} </h3>
+                                <p class = "titulo-canal"> ${video.descricao}</p>
+                                <p class = "categoria" hidden>${video.categoria} </p>
+                            </div>
+                    </li>
+                `
+            })
+        } catch(error) {
+            containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos ${error} </p>`
+        }
+    }
+
+    buscarEMostrarVideos()
+
+    const barraDePesquisa = document.querySelector('.pesquisar__input')
+
+    barraDePesquisa.addEventListener('input', filtrarPesquisa)
+
+    function filtrarPesquisa() {
+        const videos = document.querySelectorAll('.videos__item')
+        const valorFiltro = barraDePesquisa.value.toLowerCase()
+
+        videos.forEach(video => {
+            const titulo = video.querySelector('.titulo-video').textContent.toLowerCase()
+
+            video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? "block" : "none" : "block"
+        })
+    }
+
+    const botaoCategoria = document.querySelectorAll('.superior__item')
+    botaoCategoria.forEach((botao) => {
+        let nomeCategoria = botao.getAttribute('name')
+        botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria))
     })
-)
-.catch(erro => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos ${erro}</p>`
-})
+
+    function filtrarPorCategoria(filtro) {
+        const videos = document.querySelectorAll('.videos__item')
+        let valorFiltro = filtro.toLowerCase()
+        videos.forEach(video => {
+            let categoria = video.querySelector('.categoria').textContent.toLowerCase()
+
+            video.style.display =  valorFiltro ? categoria.includes(valorFiltro) ? "block" : "none" : "block"
+        })
+        
+        // for(let video of videos) {
+        //     let categoria = video.querySelector('.categoria').textContent.toLowerCase()
+        //     let valorFiltro = filtro.toLowerCase()
+
+        //     if(!categoria.includes(valorFiltro) && valorFiltro != "tudo"){
+        //         video.style.display = "none"
+        //     } else {
+        //         video.style.display = "block"
+        //     }
+        // }
+    }
