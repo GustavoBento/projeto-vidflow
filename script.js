45004c547d677194acaa6f6303c3b3270355c3e8
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2024-02-27 13:58:58 -0300

    atualizando a url do json

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const containerVideos = document.querySelector('.videos__container')
 
 async function buscarEMostrarVideos() {
     try {
-        const busca = await fetch("https://mp62eda71614a0fea8b6.free.beeceptor.com")
+        const busca = await fetch("https://mp62eda71614a0fea8b6.free.beeceptor.com/data")
         const videos = await busca.json()
             videos.forEach(video => {
                 if(video.categoria == ''){
